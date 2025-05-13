537683c0d4c194ee75277b053ee4e3e258d6afdc
Kristofer Hallin <kristofer@sunet.se>
2025-05-13 11:33:32 +0200

    New page for TXT editing.

---
+++ ui/pages/txt.py
@@ -0,0 +1,68 @@
+import requests
+from nicegui import ui, app
+from pages.common import API_URL, page_init
+
+
+def save_file(data: str) -> None:
+    """
+    Save the edited content to a file.
+    """
+    filename = ui.input("Enter filename").value
+    if not filename:
+        ui.notify("Filename cannot be empty")
+        return
+
+    with open(filename, "w") as f:
+        f.write(data)
+    ui.notify(f"File saved as {filename}")
+
+
+# A simple text editor using ui.editor
+def txt_editor(data):
+    """
+    Create a text editor with the given data.
+    """
+    editor = ui.editor(
+        value=data,
+    ).style("width: 100%; height: calc(100vh - 100px); white-space: pre-wrap;")
+
+    return editor
+
+
+def create() -> None:
+    @ui.page("/txt")
+    def result(uuid: str, filename: str) -> None:
+        """
+        Display the result of the transcription job.
+        """
+        app.add_static_files(url_path="/static", local_directory="static/")
+        response = requests.get(f"{API_URL}/transcriber/{uuid}/result")
+
+        if response.status_code != 200:
+            ui.notify("Error: Failed to get result")
+            return
+
+        data = response.content.decode("utf-8")
+
+        page_init()
+
+        txt_editor(data)
+
+        with ui.left_drawer(fixed=True):
+            with ui.row().classes("justify-end"):
+                ui.button(
+                    "My files",
+                    icon="arrow_back",
+                    color="primary",
+                    on_click=lambda: ui.navigate.to("/home"),
+                ).classes("w-full")
+                ui.button(
+                    "Export",
+                    icon="save",
+                    color="primary",
+                ).classes("w-full")
+                ui.button(
+                    "Revert",
+                    icon="undo",
+                    color="negative",
+                ).classes("w-full")
